4836c945c39866fa2d75b9203b8d40d25a9b08c2
tanase <atc.t.tanase@gmail.com>
2025-10-22 13:41:44 +0900

    no message

--- test_1017/02_javascript/3Dof/3Dof_1.js
+++ test_1017/02_javascript/3Dof/3Dof_1.js
@@ -129,7 +129,7 @@ barcodeMarker.addEventListener("markerFound", () => {
 
         // 下向き角度に応じたy軸補正（rotXが正ならplaneは下方向にズレるので、y座標を減らす）
         const correctionFactor = 0.02; // 補正量は調整可能
-        const yCorrection = (90 - rotX) * correctionFactor;
+        const yCorrection = (rotX-90) * correctionFactor;
 
         // rotY（左右）で左右方向（x軸）を補正
         const correctionFactorX = 0.02;  // 左右補正の強さ
